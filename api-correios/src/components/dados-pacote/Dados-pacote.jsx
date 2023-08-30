65b726ed0a88827100b572aedb4d7cc41f871919
Alexandregv1080p <alexandregv1999@gmail.com>
2023-08-30 14:02:54 -0300

    Update Dados-pacote.jsx

--- api-correios/src/components/dados-pacote/Dados-pacote.jsx
+++ api-correios/src/components/dados-pacote/Dados-pacote.jsx
@@ -4,14 +4,13 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { useDataContext } from "../provedor-dados/DataProvider";
+import { useNavigate } from 'react-router-dom';
 
 const BackColor = styled.div`
     background-color: #FFCD40;
     width: 100%;
     height: 965px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `
 const Retangule = styled.div`
     width: 1235px;
@@ -46,107 +45,158 @@ const FormField3 = styled.div`
     justify-content: flex-start;
     flex-wrap: wrap;
 `
-const DadosPacote = ()=>{
+const OrangeContainerRetangule = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 10px;
+    position: relative;
+`
+const OrangeRetangule = styled.div`
+    width: 400px;
+    height: 130px;
+    background-color: #cf9f1c;
+    border-radius: 40px;
+    margin-bottom: 50px;
+    transform: translateX(-50%);
+    text-align: center;
+    cursor: pointer;
+    & p{
+        color: white;
+        margin-top: 10px;
+        position: relative;
+    }
+`
+const ContainerRetangule = styled.div`
+    background-color: #FFCD40;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    margin-top: 100px;
+`
+
+const DadosPacote = () => {
+    const { senderData } = useDataContext();
+    const { receiverData } = useDataContext();
+    const { packageData, setPackageData } = useDataContext();
+
     const [loading, setLoading] = React.useState(true);
+
+    const navigate = useNavigate();
+
+    const handleVoltarClick = () => {
+        navigate('/');
+    }
+
     function handleClick() {
         setLoading(true);
     }
     return (
         <BackColor>
-            <Retangule>
-                <h1>Dados do pacote de envio</h1>
-                <Container>
-                    <FormField>
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Peso"
-                        />
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Altura"
-                        />
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Largura"
-                        />
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Comprimento"
-
-                        />
-                    </FormField>
-                    <FormField2>
-                        <FormControlLabel
-                            sx={{
-                                display: 'block',
-                            }}
-                            label="Logistica Reversa"
-                            control={
-                                <Switch
-                                    checked={loading}
-                                    onChange={() => setLoading(!loading)}
-                                    name=""
-                                    color="primary"
-                                />
-                            }
-                            
-                        />
-                        <FormControlLabel
-                            sx={{
-                                display: 'block',
-                            }}
-                            control={
-                                <Switch
-                                    checked={loading}
-                                    onChange={() => setLoading(!loading)}
-                                    name=""
-                                    color="primary"
-                                />
-                            }
-                            label="Aviso de recebimento"
-                        />
-                        <FormControlLabel
-                            sx={{
-                                display: 'block',
-                            }}
-                            control={
-                                <Switch
-                                    checked={loading}
-                                    onChange={() => setLoading(!loading)}
-                                    name=""
-                                    color="primary"
-                                />
-                            }
-                            label="Mãos próprias"
-                        />
-                    </FormField2>
-                    <FormField3>
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Valor da mercadoria"
-                        />
-                        <TextField
-                            required
-                            id="outlined-required"
-                            label="Quantidade de items"
-                        />
-                         <TextField
-                            id="outlined-multiline-static"
-                            label="Descrição dos items"
-                            multiline
-                            rows={10}
-                            fullWidth
+            <OrangeContainerRetangule>
+                <OrangeRetangule onClick={handleVoltarClick}>
+                    <h3>Dados Origem</h3>
+                    <p>{senderData.fullname} - {senderData.cpf}</p>
+                    <p>{senderData.address.cep} - {senderData.address.state} - {senderData.address.uf} - {senderData.address.city}</p>
+                    <p>{senderData.address.neighborhood} - {senderData.address.street} - {senderData.address.number} - {senderData.address.complement}</p>
+                </OrangeRetangule>
+            </OrangeContainerRetangule>
+            <ContainerRetangule>
+                <Retangule>
+                    <h1>Dados do pacote de envio</h1>
+                    <Container>
+                        <FormField>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Peso"
                             />
-                    </FormField3>
-                </Container>
-                <Button variant="contained">Avançar</Button>
-                
-            </Retangule>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Altura"
+                            />
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Largura"
+                            />
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Comprimento"
+
+                            />
+                        </FormField>
+                        <FormField2>
+                            <FormControlLabel
+                                sx={{
+                                    display: 'block',
+                                }}
+                                label="Logistica Reversa"
+                                control={
+                                    <Switch
+                                        checked={loading}
+                                        onChange={() => setLoading(!loading)}
+                                        name=""
+                                        color="primary"
+                                    />
+                                }
+
+                            />
+                            <FormControlLabel
+                                sx={{
+                                    display: 'block',
+                                }}
+                                control={
+                                    <Switch
+                                        checked={loading}
+                                        onChange={() => setLoading(!loading)}
+                                        name=""
+                                        color="primary"
+                                    />
+                                }
+                                label="Aviso de recebimento"
+                            />
+                            <FormControlLabel
+                                sx={{
+                                    display: 'block',
+                                }}
+                                control={
+                                    <Switch
+                                        checked={loading}
+                                        onChange={() => setLoading(!loading)}
+                                        name=""
+                                        color="primary"
+                                    />
+                                }
+                                label="Mãos próprias"
+                            />
+                        </FormField2>
+                        <FormField3>
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Valor da mercadoria"
+                            />
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Quantidade de items"
+                            />
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Descrição dos items"
+                                multiline
+                                rows={10}
+                                fullWidth
+                            />
+                        </FormField3>
+                    </Container>
+                    <Button variant="contained">Avançar</Button>
+
+                </Retangule>
+            </ContainerRetangule>
         </BackColor>
     )
 }
