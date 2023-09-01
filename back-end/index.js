ff6194261e41cac74827d4ef7099cdc1af650768
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-01 16:16:18 -0300

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Importe o body-parser
 const app = express();
-const port = 3001; 
+const port = 3001;
 
-app.post('/api/calculate-shipping', async (req, res) => {
-    try {
-      const { sender, receiver, package } = req.body;
-  
-      const calculatedShipping = calculateShipping(sender, receiver, package);
-        res.json(calculatedShipping);
-    } catch (error) {
-      console.error('Erro no cálculo do frete:', error);
-      res.status(500).json({ error: 'Erro no servidor' });
-    }
-  });
-  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('api/shipping_calculate', async (req, res) => {
+  try {
+    const { sender, receiver, package } = req.body;
+    console.log("Testando");
+  } catch (error) {
+    console.error('Erro no cálculo do frete:', error);
+    res.status(500).json({ error: 'Erro no servidor' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
+});
